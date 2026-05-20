69f9fd42cfcb7d41ae77b2f5106dbbbbeff361ce
Рома Авраменко <www.kioma220@gmail.com>
2026-05-20 17:06:38 +0300

    Tranfer Med

---
+++ BuisnessLogic/UseCases/TransfersUseCasses/Commands/TransferMoneyCommand.cs
@@ -0,0 +1,58 @@
+﻿using BuisnessLogic.Entities;
+using BuisnessLogic.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BuisnessLogic.UseCases.TransfersUseCasses.Commands
+{
+    public sealed record TransferMoneyCommand(Guid UserId,
+        decimal Amount,
+        string Description,
+        DateTime Date,
+        Guid FromAccountId,
+        Guid ToAccountId)
+        : IRequest;
+
+    public class TransferMoneyCommandHandler(ITransferRepository transferRepository,
+        IAccountRepository accountRepository)
+        : IRequestHandler<TransferMoneyCommand>
+    {
+        public async Task Handle(TransferMoneyCommand request, CancellationToken cancellationToken)
+        {
+            var fromAccount = await accountRepository.GetById(request.UserId, request.FromAccountId);
+            var toAccount = await accountRepository.GetById(request.UserId, request.ToAccountId);
+
+            if (fromAccount is null)
+            {
+                throw new ArgumentException($"No Account with Id: {request.FromAccountId}");
+            }
+
+
+            if (toAccount is null)
+            {
+                throw new ArgumentException($"No Account with Id: {request.ToAccountId}");
+            }
+
+            fromAccount.RemoveFromBalance(request.Amount);
+            toAccount.AddToBalance(request.Amount);
+
+            await transferRepository.Add(new Transfer
+            {
+                Amount = request.Amount,
+                Description = request.Description,
+                Date = request.Date,
+                FromAccountId = request.FromAccountId,
+                ToAccountId = request.ToAccountId,
+                UserId = request.UserId
+            });
+
+            await accountRepository.Update(fromAccount);
+            await accountRepository.Update(toAccount);
+        }
+    }
+}
---
+++ BuisnessLogic/UseCases/TransfersUseCasses/Queries/GetAllTransfersQuery.cs
@@ -0,0 +1,26 @@
+﻿using BuisnessLogic.Entities;
+using BuisnessLogic.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLogic.UseCases.TransfersUseCasses.Queries
+{
+    public sealed record GetAllTransfersQuery(Guid UserId)
+        : IRequest<IEnumerable<Transfer>>;
+
+    public class GetAllTransfersQueryHandler(ITransferRepository transferRepository)
+        : IRequestHandler<GetAllTransfersQuery, IEnumerable<Transfer>>
+    {
+        public async Task<IEnumerable<Transfer>> Handle(GetAllTransfersQuery request, CancellationToken cancellationToken)
+        {
+            return await transferRepository.GetAll(request.UserId,
+                x => x.FromAccount,
+                x => x.ToAccount,
+                x => x.User);
+        }
+    }
+}
