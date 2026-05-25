388863c0434c896c887a9193213435e9d73b2c93
Рома Авраменко <www.kioma220@gmail.com>
2026-05-25 08:52:07 +0300

    UC DI

--- BuisnessLogic/UseCases/DependencyInjection.cs
+++ BuisnessLogic/UseCases/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuisnessLogic.BudgetService;
+using BuisnessLogic.DebtInterestCalculator;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BuisnessLogic.UseCases
 {
@@ -7,6 +9,22 @@ namespace BuisnessLogic.UseCases
         public static IServiceCollection AddUseCassess(this
             IServiceCollection services)
         {
+            services.AddSingleton<IBudgetSpecificationsCreator, BudgetAccountSpecificationsCreator>();
+            services.AddSingleton<IBudgetSpecificationsCreator, BudgetCategorySpecificationsCreator>();
+            services.AddSingleton<IBudgetSpecificationsCreator, BudgetFamilyMemberSpecificationsCreator>();
+            services.AddSingleton<IBudgetSpecificationsCreator, BudgetTransactionTagSpecificationsCreator>();
+            services.AddSingleton<IBudgetSpecificationsCreator, BudgetTransactionTypeSpecificationsCreator>();
+
+            services.AddSingleton<IBudgetSpecificationsCreatorsProvider, BudgetSpecificationsCreatorsProvider>();
+            services.AddSingleton<IBudgetSpecificationsExtender, BudgetSpecificationsExtender>();
+
+            services.AddSingleton<IDebtInterestCalculator, ComplexInterestCalcilator>();
+            services.AddSingleton<IDebtInterestCalculator, FixedInterestCalcilator>();
+            services.AddSingleton<IDebtInterestCalculator, NoneInterestCalculator>();
+            services.AddSingleton<IDebtInterestCalculator, SimpleInterestCalculator>();
+
+            services.AddSingleton<IDebtInterestCalculatorProvider, DebtInterestCalculatorProvider>();
+
             services.AddMediatR(conf =>
 
                 conf.RegisterServicesFromAssembly(typeof(DependencyInjection)
--- UI/MauiProgram.cs
+++ UI/MauiProgram.cs
@@ -36,44 +36,14 @@ namespace UI
 
             builder.Services.AddDbContextFactory<FinansiesDbContext, FinansiesDbContextFactory>();
 
-            builder.Services.AddSingleton<IBudgetSpecificationsCreator, BudgetAccountSpecificationsCreator>();
-            builder.Services.AddSingleton<IBudgetSpecificationsCreator, BudgetCategorySpecificationsCreator>();
-            builder.Services.AddSingleton<IBudgetSpecificationsCreator, BudgetFamilyMemberSpecificationsCreator>();
-            builder.Services.AddSingleton<IBudgetSpecificationsCreator, BudgetTransactionTagSpecificationsCreator>();
-            builder.Services.AddSingleton<IBudgetSpecificationsCreator, BudgetTransactionTypeSpecificationsCreator>();
-
-            builder.Services.AddSingleton<IBudgetSpecificationsCreatorsProvider, BudgetSpecificationsCreatorsProvider>();
-            builder.Services.AddSingleton<IBudgetSpecificationsExtender, BudgetSpecificationsExtender>();
-
-            builder.Services.AddSingleton<IDebtInterestCalculator, ComplexInterestCalcilator>();
-            builder.Services.AddSingleton<IDebtInterestCalculator, FixedInterestCalcilator>();
-            builder.Services.AddSingleton<IDebtInterestCalculator, NoneInterestCalculator>();
-            builder.Services.AddSingleton<IDebtInterestCalculator, SimpleInterestCalculator>();
-
-            builder.Services.AddSingleton<IDebtInterestCalculatorProvider, DebtInterestCalculatorProvider>();
-
             builder.Services.AddSingleton<IUserContext, UserContext>();
 
-            //builder.Services.AddTransient<IAccountRepository, AccountRepository>();
-            //builder.Services.AddTransient<IBudgetRepository, BudgetRepository>();
-            //builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
-            //builder.Services.AddTransient<IDebtRepository, DebtRepository>();
-            //builder.Services.AddTransient<IFamilyMemberRepository, FamilyMemberRepository>();
-            //builder.Services.AddTransient<ITransactionRepository, TransactionRepository>();
-            //builder.Services.AddTransient<IPlannedTransactionRepository, PlannedTransactionRepository>();
-            //builder.Services.AddTransient<ITransactionTagRepository, TransactionTagRepository>();
-            //builder.Services.AddTransient<ITransferRepository, TransferRepository>();
-            //builder.Services.AddTransient<IUserRepository, UserRepository>();
 
-            builder.Services.AddRepositories();
-
-            builder.Services.AddUseCassess();
-
-            builder.Services.AddViewModels();
-
-            builder.Services.AddViews();
-
-            builder.Services.AddPopUps();
+            builder.Services.AddRepositories()
+                .AddUseCassess()
+                .AddViewModels()
+                .AddViews()
+                .AddPopUps();
 
 #if DEBUG
             builder.Logging.AddDebug();
