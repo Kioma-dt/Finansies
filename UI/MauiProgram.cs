20ae6b8ebfa5dc4c2474c84322f8b10134bba75b
Рома Авраменко <www.kioma220@gmail.com>
2026-05-30 16:47:52 +0300

    Icno + Name + FullScreen

--- UI/MauiProgram.cs
+++ UI/MauiProgram.cs
@@ -5,20 +5,26 @@ using BuisnessLogic.UseCases;
 using CommunityToolkit.Maui;
 using DataAccess;
 using DataAccess.RepositoriesImplementation;
+using LiveChartsCore.SkiaSharpView.Maui;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using UI.Popups;
-
+using Microsoft.Maui.LifecycleEvents;
 using SkiaSharp.Views.Maui.Controls.Hosting;
-using UI.PopUps.DependencyInjection;
-using UI.ViewModels;
-using UI.Views.DependencyInjection;
-using UI.ViewModels.DependencyInjection;
-using LiveChartsCore.SkiaSharpView.Maui;
-using UI.Statistics;
 using UI.OrderingServices;
+using UI.Popups;
+using UI.PopUps.DependencyInjection;
+using UI.Statistics;
+using UI.ViewModels;
+using UI.ViewModels.DependencyInjection;
+using UI.Views.DependencyInjection;
+
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+#endif
+
 
 namespace UI
 {
@@ -56,6 +62,29 @@ namespace UI
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
+
+            builder.ConfigureLifecycleEvents(events =>
+            {
+#if WINDOWS
+    events.AddWindows(w =>
+    {
+        w.OnWindowCreated(window =>
+        {//If you need to completely hide the minimized maximized close button, you need to set this value to false.
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+            WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            var _appWindow = AppWindow.GetFromWindowId(myWndId);
+            _appWindow.SetPresenter(AppWindowPresenterKind.Overlapped);
+
+            switch (_appWindow.Presenter)
+                    {
+                        case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                            overlappedPresenter.Maximize();
+                            break;
+                    }
+        });
+    });
+#endif
+            });
 
             return builder.Build();
         }
